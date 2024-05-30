a1c5f41ac6660988c0a88ba94c36cafc0d6f16b3
Aishan <121613744+AhmedAishan@users.noreply.github.com>
2024-05-30 20:47:55 +0530

    updated logic script

--- script.js
+++ script.js
@@ -4,44 +4,76 @@ const eventForm = document.getElementById('form');
 const eventval = document.getElementById('event');
 const dateval = document.getElementById('date');
 const eventHead = document.querySelector('.event-head');
-const tick = document.querySelector('.tick');
+const eventTime = document.querySelector('.tick');
 
 eventForm.addEventListener('submit', function (e) {
   e.preventDefault();
-  let time = new Date().getSeconds();
-  console.log(time);
-  const eventDate = new Date(dateval.value);
 
-  // eventHead.innerText = eventval.value;
+  // future date
+  // const futureDate = new Date(dateval.value);
+  // futureDate.setHours(0, 0, 0);
+  // console.log(
+  //   'test',
+  //   `${futureDate.getHours()} : ${futureDate.getMinutes()} : ${futureDate.getSeconds()}`
+  // );
+  // console.log('future', futureDate);
+  // const currentDate = new Date();
+  // console.log('present', currentDate);
+
+  // setting the event title
+  eventHead.innerText = eventval.value;
+
+  // calculate difference
+  // const calcDifference = (future, present) => future - present;
+
+  // calculating remaing time for the event
+  // const remainingTime = {
+  //   days: calcDifference() / (1000 * 60 * 60 * 24),
+  //   hours: (calcDifference() / (1000 * 60 * 60)) % 24,
+  //   minutes: (calcDifference() / (1000 * 60)) % 60,
+  //   seconds: (calcDifference() / 1000) % 24,
+  // };
+
+  // console.log(Math.round(Math.abs(remainingTime.days)));
+  // console.log(Math.floor(remainingTime.hours));
+  // console.log(Math.floor(remainingTime.minutes));
+  // console.log(Math.floor(Math.abs(remainingTime.seconds)));
+
+  // setInterval(function () {
+  //   eventTime.innerText = `${Math.round(
+  //     Math.abs(remainingTime.days)
+  //   )} : ${Math.floor(remainingTime.hours)} : ${Math.floor(
+  //     remainingTime.minutes
+  //   )} : ${Math.floor(
+  //     Math.abs(
+  //       remainingTime.seconds > 0
+  //         ? remainingTime.seconds--
+  //         : (remainingTime.seconds = 59)
+  //     )
+  //   )}`;
+  // }, 1000);
 
   setInterval(function () {
-    let seconds = time % 60;
-    let minutes = Math.floor(time / 60);
-    let hour = Math.floor(minutes / 60);
-    let day = Math.floor(hour / 24);
+    const futureDate = new Date(dateval.value);
+    futureDate.setHours(0, 0, 0);
+    const currentDate = new Date();
+    const calcDifference = () => futureDate - currentDate;
+    const remainingTime = {
+      days: calcDifference() / (1000 * 60 * 60 * 24),
+      hours: (calcDifference() / (1000 * 60 * 60)) % 24,
+      minutes: (calcDifference() / (1000 * 60)) % 60,
+      seconds: (calcDifference() / 1000) % 60,
+    };
 
-    const curDate = new Date(
-      eventDate.getFullYear(),
-      eventDate.getMonth(),
-      eventDate.getDate(),
-      hour,
-      minutes,
-      seconds
-    );
-
-    let now = new Intl.DateTimeFormat(navigator.language, {
-      minute: 'numeric',
-      second: 'numeric',
-      hour: 'numeric',
-      day: 'numeric',
-      year: 'numeric',
-      month: 'long',
-    }).format(curDate);
-    // tick.innerText = now;
-    console.log(now);
+    eventTime.innerText = `${Math.round(
+      Math.abs(remainingTime.days)
+    )} : ${Math.floor(remainingTime.hours)} : ${Math.floor(
+      remainingTime.minutes
+    )} : ${Math.floor(Math.abs(remainingTime.seconds))}`;
   }, 1000);
 });
 
+/**
 const futureDate = new Date(2024, 9, 18);
 const currentDate = new Date();
 
@@ -59,3 +91,4 @@ console.log(
   'time remaining ',
   `${daysR} : ${hoursR} : ${minutesR} : ${secondsR}`
 );
+ */
